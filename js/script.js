330a9ec480cb4b2af2dd766884f79c327a724511
Maruf <maruf.zakirov.20042004@gmail.com>
2021-06-07 17:50:59 +0500

    ,

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
 
+
+    const tl = gsap.timeline()
+    gsap.from('.plane__img', { margin: '0 0 0 auto', scrollTrigger: {
+        trigger: '.plane',
+        start: 'top center',
+        end: 'center center' ,
+        scrub: true,
+    }})
+    
 
 const headerSwiper = new Swiper('.start__slider',{
     wrapperClass: "start__wrapper",
@@ -14,6 +24,37 @@ const headerSwiper = new Swiper('.start__slider',{
     },
 })  
 
+const firstDaySwiper = new Swiper('.come__slider',{
+    wrapperClass: "come__wrapper",
+    slideClass: "come__slide",
+    slideActiveClass: "come__slide_active",
+    slidesPerView: '2',
+    spaceBetween: 32,
+    // loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: '.btn_next',
+        prevEl: '.btn_prev',
+    },
+    autoHeight: true
+})  
+
+const sourceSlider = new Swiper('.source__slider',{
+    wrapperClass: "source__wrapper",
+    slideClass: "source__slide",
+    slideActiveClass: "source__slide_active",
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    // loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: '.btn_next',
+        prevEl: '.btn_prev',
+    },
+    autoHeight: true,
+    
+})  
+
 const header = document.querySelector('header');
 const startBody = document.querySelector('.start__body');
 const startSlider = document.querySelector('.start__slider');
@@ -24,10 +65,17 @@ startBody.style.paddingTop =`${header.clientHeight}px`
 
 window.addEventListener('load',function () {  
     if (window.innerWidth > 992) {
-        var scene = document.getElementById('scene');
-        var parallaxInstance = new Parallax(scene);
+        // var scene = document.getElementById('scene');
+        // var parallaxInstance = new Parallax(scene);
     } else {
         startSlider.classList.add('container')
+    }
+})
+window.addEventListener('scroll',function () {  
+    if (window.pageYOffset > 50) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
     }
 })
 
